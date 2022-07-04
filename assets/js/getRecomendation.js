e50b9bbb4eb754829a8a8e26495c5386be9e0649
an-halim <halimbla2@gmail.com>
2022-07-05 06:07:24 +0700

    create logic for recomendation

---
+++ assets/js/getRecomendation.js
@@ -0,0 +1,319 @@
+let contentList = [
+    {
+        name: 'Ahmad Nur Halim',
+        photo: '/assets/img/AhmadNurHalim.jpg',
+        province: 'Jateng',
+        articles : [
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Borobudur',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Batik Jawa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Rumah Joglo',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Keris',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Gamelan',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Kuda Lumping',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Wayang Kulit',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Kethoprak',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+        ]
+    },
+    {
+        name: 'Betharia Prenesti Nur Pafitri',
+        photo: '/assets/img/AhmadNurHalim.jpg',
+        province: 'Bali',
+        articles : [
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa1',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa2',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa3',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa4',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa5',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa6',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa7',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa8',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa8',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+        ]
+    },
+    {
+        name: 'Okta Eka Pratama',
+        photo: '/assets/img/okta.jpg',
+        province: 'Kalimantan Selatan',
+        articles : [
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa12',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa31',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa311',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa124',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa4124',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa4124',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa4125',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa215',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa5215',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+        ]
+    },
+    {
+        name: 'Satrio Wibisono',
+        photo: '/assets/img/WibiSini.jpg',
+        province: 'NTT',
+        articles : [
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa5125',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa52115',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawa51125',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jaw2a',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Ja21wa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Ja51wa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jaw1255a',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Ja211wa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jaw1255a',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+        ]
+    },
+    {
+        name: 'Ade Indra Yuda Pratama',
+        photo: '/assets/img/AdeIndra.jpeg',
+        province: 'Sulawesi',
+        articles : [
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jafswa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jfsdawa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jarewwa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jtawa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara rJawa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawqa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jaeqwa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jaeqwa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+            {
+                articelPath: '/jateng/aksara-jawa',
+                tittle: 'Aksara Jawrqxa',
+                articlePhoto: '/assets/img/gamelan.jpg'
+            },
+        ]
+    }
+]
+
+
+/**
+ * Method untuk mendapatkan random article dari data array of object yang sudah disiapkan
+ * @param {*} count Berapa banyak artikel yang anda inginkan
+ * @returns 
+ * Nilai atau data kembalian berupa array of object
+ */
+
+const getRecomendation = (count) => {
+    let randomizeContents = [];
+    let randomAuthor, randomArticle, randomizeContent;
+    
+    for(let i = 0; i < count; i++){
+
+        // avoid same recomendation article
+        do{
+            randomAuthor = Math.floor(Math.random() * contentList.length)
+            randomArticle = Math.floor(Math.random() * contentList[randomAuthor].articles.length)
+
+            randomizeContent = {
+                name: contentList[randomAuthor].name,
+                photo: contentList[randomAuthor].photo,
+                province: contentList[randomAuthor].province,
+                article: contentList[randomAuthor].articles[randomArticle]
+            }
+
+        }while(JSON.stringify(randomizeContents).includes(randomizeContent.article.tittle) && JSON.stringify(randomizeContents).includes(randomizeContent.name))
+
+
+        randomizeContents.push(randomizeContent)
+    }
+
+    return randomizeContents
+}
+
+let recomentationsEL = document.querySelectorAll('.recomendation-item')
+let parentUrl = window.location.origin;
+let data = getRecomendation(recomentationsEL.length)
+
+// append random recomendation to each element
+for(let i = 0; i < recomentationsEL.length; i++){
+    var authorName = recomentationsEL[i].querySelector('#author');
+    var authorPhoto = recomentationsEL[i].querySelector('#author-photo');
+    var article = recomentationsEL[i].querySelector('#article-tittle');
+    var articlePhoto = recomentationsEL[i].querySelector('#article-image')
+    var articleProv = recomentationsEL[i].querySelector('#articel-prov')
+    
+    authorName.innerHTML = data[i].name.length > 10 ? data[i].name.slice(0, 10).trimEnd() + '...' : data[i].name
+    authorPhoto.src =parentUrl + data[i].photo
+    articleProv.innerHTML = data[i].province.length > 6 ? data[i].province.slice(0, 6).trimEnd() + '...' : data[i].province
+    article.innerHTML = data[i].article.tittle.length > 11 ? data[i].article.tittle.slice(0, 11).trimEnd().toUpperCase() + '...' : data[i].article.tittle.toUpperCase()
+    articlePhoto.src = parentUrl + data[i].article.articlePhoto
+
+    recomentationsEL[i].setAttribute('onclick', `window.location.href = '${parentUrl+data[i].article.articelPath}'`);
+    console.log(data[i])
+}

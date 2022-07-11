9ef376d3f507ff5bc2bf27d36c24da0f22d97179
Wibi Sono <wibicrull35@gmail.com>
2022-07-11 20:54:32 +0700

    update cont sub page , css, js

--- ntt/Alat-Musik/assets/js/script.js
+++ ntt/Alat-Musik/assets/js/script.js
@@ -4,60 +4,113 @@ let choosedEmoji = "";
 
 
 window.addEventListener('load', () =>{
-    let state = localStorage.getItem('theme');
-    console.log(state)
-    let body = document.body;
-    let nav = document.querySelector('nav')
-    let headColor = document.querySelector("head > meta:nth-child(4)");
-    
-    let form = document.querySelector('form')
-    if(state == 'dark'){
-        body.classList.toggle("dark-mode");
-        nav.classList.toggle("dark-mode")
-        nav.classList.toggle("navbar-dark")
-        headColor.content = '#1d1d1d'
-        themeBtn.innerHTML = '<img src="./assets/img/brightness.png" class="img-darkmode" alt="ligth mode">'
-    }
+  let state = localStorage.getItem('theme');
+  let headColor = document.querySelector("head > meta:nth-child(4)");
+  let nav = document.querySelector('nav')
+  
+  if(state == 'dark'){
+    nav.classList.toggle('navbar-dark')
+    headColor.content = '#1d1d1d'
+    themeBtn.innerHTML = '<img src="./assets/img/brightness.png" class="img-darkmode" alt="ligth mode">'
+    $('head').append('<link rel="stylesheet" href="./assets/css/darkmode.css">');
+  }
 })
 
 mobileNav.addEventListener('click', () => {
-  console.log('hai im clicked!')
   let navContainer = document.querySelector('.navbar');
   navContainer.classList.toggle('bg-transparent')
 })
 
 themeBtn.addEventListener('click', () => {
-    console.log("Clicked!")
-    let headColor = document.querySelector("head > meta:nth-child(4)");
-    let state = localStorage.getItem('theme');
-    let body = document.body;
-    let nav = document.querySelector('nav')
-    let form = document.querySelector('form')
-    if(state == 'dark'){
-        localStorage.removeItem('theme');
-        themeBtn.innerHTML = '<img src="./assets/img/night-mode.png" class="img-darkmode" alt="dark mode">'
-        headColor.content = '#0dcaf0'
-    }else{
-        localStorage.setItem('theme', 'dark');
-        themeBtn.innerHTML = '<img src="./assets/img/brightness.png" class="img-darkmode" alt="ligth mode">'
-        headColor.content = '#1d1d1d'
-    }
-    body.classList.toggle("dark-mode");
-    nav.classList.toggle("dark-mode");
-    nav.classList.toggle("navbar-dark");
-    
+  let nav = document.querySelector('nav')
+  let headColor = document.querySelector("head > meta:nth-child(4)");
+  let state = localStorage.getItem('theme');
+  let isDarkMode = $('link[href="./assets/css/darkmode.css"]');
+
+  if(state == 'dark'){
+    nav.classList.toggle('navbar-dark')
+    localStorage.removeItem('theme');
+    themeBtn.innerHTML = '<img src="./assets/img/night-mode.png" class="img-darkmode" alt="dark mode">'
+    headColor.content = '#0dcaf0'
+    isDarkMode.remove();
+  }else{
+    nav.classList.toggle('navbar-dark')
+    localStorage.setItem('theme', 'dark');
+    themeBtn.innerHTML = '<img src="./assets/img/brightness.png" class="img-darkmode" alt="ligth mode">'
+    headColor.content = '#1d1d1d'
+    $('head').append('<link rel="stylesheet" href="./assets/css/darkmode.css">');
+
+  }
+  
 })
+
+// function scroll bar
+
+function scrollProgressBar() {
+  var getMax = function () {
+    return $(document).height() - $(window).height();
+  };
+
+  var getValue = function () {
+    return $(window).scrollTop();
+  };
+
+  var progressBar = $(".progress-bar"),
+    max = getMax(),
+    value,
+    width;
+
+  var getWidth = function () {
+    // Calculate width in percentage
+    value = getValue();
+    width = (value / max) * 100;
+    width = width + "%";
+    return width;
+  };
+
+  var setWidth = function () {
+    progressBar.css({ width: getWidth() });
+  };
+
+  $(document).on("scroll", setWidth);
+  $(window).on("resize", function () {
+    // Need to reset max
+    max = getMax();
+    setWidth();
+  });
+}
+
+
 
 
 const handleFeedback = () => {
-    let btn = document.getElementById('send-feedback')
-    let name = document.getElementById('name');
-    let comment = document.getElementById('feedback-placeholder');
-    btn.href = `mailto:halimbla2@gmail.com?subject=${name.value} merasa ${choosedEmoji} dengan artikel&body=${comment.value}`
+  let emojies = document.querySelectorAll('#ic-reaction');
+  let btn = document.getElementById('send-feedback')
+  let name = document.getElementById('name');
+  let comment = document.getElementById('feedback-placeholder');
+  let feedbackRes = document.querySelector('.feedback-response');
+  
+
+  if(name.value.length > 0 && comment.value.length > 0){
+    emojies.forEach((e) => {
+      e.style.filter = 'grayscale(0%)'
+      e.style.fontSize = 'large'
+    })
+    btn.href = `mailto:halimbla2@gmail.com?subject=${name.value} merasa ${choosedEmoji} dengan artikel&body=${comment.value}`;
+    let containerFeedback = document.getElementById('container-feedback');
+    containerFeedback.classList.toggle('d-none')
+    feedbackRes.classList.toggle('d-none')
+  }
 }
 
 const handleEmoji = (event) => {
-  
+  let emojies = document.querySelectorAll('#ic-reaction');
+
+  emojies.forEach((e) => {
+    e.innerHTML !== event.innerHTML ? e.style.filter = 'grayscale(100%)' : e.style.filter = 'grayscale(0%)'
+    e.innerHTML === event.innerHTML ? e.style.fontSize = 'x-large' : e.style.fontSize = 'large' 
+  })
+
   switch(event.innerHTML.trim()){
     case '😒':
       choosedEmoji = 'tidak terbantu';
@@ -74,28 +127,24 @@ const handleEmoji = (event) => {
     default:
       choosedEmoji = '';
   }
-
-  console.log(choosedEmoji)
+  
   let containerFeedback = document.getElementById('container-feedback');
-  containerFeedback.classList.toggle('disable')
+  containerFeedback.classList.toggle('d-none')
 }
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+// go top
+const scrollFunction = () => {
+  const goTOp = document.getElementById("go-top");
+  
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+    goTOp.style.display = "block";
+  else
+    goTOp.style.display = "none";
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+window.onscroll = scrollFunction
+
+const topFunction = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
@@ -104,12 +153,19 @@ function topFunction() {
 document.querySelector('.main-img').addEventListener('mouseover', () => {
   document.querySelector('.share-to').classList.remove('disable')
 })
+
 document.querySelector('.main-img').addEventListener('mouseleave', () => {
   document.querySelector('.share-to').classList.add('disable')
 })
+
 $(document).ready(function(){
-    $("#myModal").modal('show');
+  // preloader
+  $(".preloader").fadeOut();
+
+  // scrollprogresbar
+  scrollProgressBar();
 });
+
 $(document).scroll(function () {
   var $nav = $(".sticky-top");
   if(localStorage.getItem('theme') != 'dark')
@@ -118,23 +174,3 @@ $(document).scroll(function () {
     $nav.toggleClass('shadow', $(this).scrollTop() > $nav.height());
 });
 
-
-document.getElementById('btn-search').addEventListener('click', (e) =>{
-  document.querySelector('input').classList.toggle('disable')
-  document.getElementById('btn-search').classList.toggle('disable');
-  document.querySelector('input').focus();
-})
-document.getElementById('search-container').addEventListener('input', (e) =>{
-  let searchValue = e.target.value;
-  let paragrafContainer = document.querySelectorAll('p');
-  for(p of paragrafContainer){
-      console.log(p.innerHTML.includes(searchValue))
-  }
-}, true)
-document.querySelector('input').addEventListener('keypress', (e) => {
-  if(e.key == 'Enter'){
-      document.getElementById('search-container').classList.toggle('disable')
-      document.getElementById('btn-search').classList.toggle('disable');
-  }
-
-})

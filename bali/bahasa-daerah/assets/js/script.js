526d0d913e580344ac6f2dff34900dbe70e4828e
an-halim <halimbla2@gmail.com>
2022-07-18 13:53:41 +0700

    fix bug go top

--- bali/bahasa-daerah/assets/js/script.js
+++ bali/bahasa-daerah/assets/js/script.js
@@ -116,7 +116,7 @@ const handleEmoji = (event) => {
 }
 
 //Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("go-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
